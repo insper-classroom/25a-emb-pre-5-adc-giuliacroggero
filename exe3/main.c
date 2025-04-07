06ec740e2702c9b91aaa31a1cf44b2f77a3bef86
Giulia <giuliar1@al.insper.edu.br>
2025-04-07 12:17:41 -0300

    ex2e3.2

--- exe3/main.c
+++ exe3/main.c
@@ -1,39 +1,70 @@
-void process_task(void *p) {
-    #define WINDOW_SIZE 5
-    static int window[WINDOW_SIZE] = {0};
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+#include <queue.h>
 
-    static int idx = 0;
+#include "pico/stdlib.h"
+#include <stdio.h>
 
-    // Soma corrente de todos os valores no buffer
-    static int sum = 0;
+#include "data.h"
+QueueHandle_t xQueueData;
 
-    // Quantas amostras já recebemos (no início, menos de 5)
-    static int samplesCount = 0;
+// não mexer! Alimenta a fila com os dados do sinal
+void data_task(void *p) {
+    vTaskDelay(pdMS_TO_TICKS(400));
 
-    int data = 0;
+    int data_len = sizeof(sine_wave_four_cycles) / sizeof(sine_wave_four_cycles[0]);
+    for (int i = 0; i < data_len; i++) {
+        xQueueSend(xQueueData, &sine_wave_four_cycles[i], 1000000);
+    }
 
     while (true) {
-        if (xQueueReceive(xQueueData, &data, pdMS_TO_TICKS(100))) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void process_task(void *p) {
+    int data = 0;
+    int buffer[5] = {0,0,0,0,0};
+    int index = 0;
+    int soma = 0;
+    int count = 0;
+
+    while (true) {
+        if (xQueueReceive(xQueueData, &data, 100)) {
+
+            soma = 0;
             
-            sum -= window[idx];       
-            sum += data;             
-            window[idx] = data;     
+            buffer[index] = data;
 
-
-            idx = (idx + 1) % WINDOW_SIZE;
-
- 
-            if (samplesCount < WINDOW_SIZE) {
-                samplesCount++;
+            for (int i = 0; i <= 4; i++){
+                soma = soma + buffer[i];
             }
+            index = index + 1;
 
+            if (index == 5){
+                index = 0;
+                
+            }
+            
+            int media_movel = soma / 5;
 
-            int filteredValue = sum / samplesCount;
+            printf("Dado filtrado: %d\n", media_movel);
 
-
-            printf("%d \n", filteredValue);
-
+            // deixar esse delay!
             vTaskDelay(pdMS_TO_TICKS(50));
         }
     }
+}
+
+int main() {
+    stdio_init_all();
+    xQueueData = xQueueCreate(64, sizeof(int));
+    xTaskCreate(data_task, "Data task ", 4096, NULL, 1, NULL);
+    xTaskCreate(process_task, "Process task", 4096, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
+    while (true)
+        ;
 }
