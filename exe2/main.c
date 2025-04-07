c7fb483dcf5e23a316c54ba3b3d0e2ff668f6f44
Giulia <giuliar1@al.insper.edu.br>
2025-04-07 12:09:40 -0300

    ex2e3

--- exe2/main.c
+++ exe2/main.c
@@ -7,17 +7,36 @@
 #include <stdio.h>
 #include "hardware/pwm.h"
 
-const int PWM_0_PIN = 4;
+const int PWM_R_PIN = 4;  
+const int PWM_G_PIN = 5;  
 
 void led_task(void *p) {
-    gpio_set_function(PWM_0_PIN, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
-    pwm_set_clkdiv(slice_num, 125);
-    pwm_set_wrap(slice_num, 100);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 80);
-    pwm_set_enabled(slice_num, true);
+
+    gpio_set_function(PWM_R_PIN, GPIO_FUNC_PWM);
+    uint slice_num_r = pwm_gpio_to_slice_num(PWM_R_PIN);
+
+
+    pwm_set_clkdiv(slice_num_r, 125);
+
+
+    pwm_set_wrap(slice_num_r, 100);
+
+    pwm_set_chan_level(slice_num_r, PWM_CHAN_A, 80);
+
+    pwm_set_enabled(slice_num_r, true);
+
+    gpio_set_function(PWM_G_PIN, GPIO_FUNC_PWM);
+    uint slice_num_g = pwm_gpio_to_slice_num(PWM_G_PIN);
+
+    pwm_set_clkdiv(slice_num_g, 125);
+    pwm_set_wrap(slice_num_g, 100);
+
+    pwm_set_chan_level(slice_num_g, PWM_CHAN_A, 20);
+
+    pwm_set_enabled(slice_num_g, true);
 
     while (true) {
+       
     }
 }
 
@@ -25,10 +44,11 @@ int main() {
     stdio_init_all();
     printf("Start RTOS \n");
 
-    xTaskCreate(led_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
-    while (true)
-        ;
+    while (true) {
+       
+    }
 }
--- exe3/main.c
+++ exe3/main.c
@@ -1,54 +1,39 @@
-#include <FreeRTOS.h>
-#include <task.h>
-#include <semphr.h>
-#include <queue.h>
-
-#include "pico/stdlib.h"
-#include <stdio.h>
-
-#include "data.h"
-QueueHandle_t xQueueData;
-
-// não mexer! Alimenta a fila com os dados do sinal
-void data_task(void *p) {
-    vTaskDelay(pdMS_TO_TICKS(400));
-
-    int data_len = sizeof(sine_wave_four_cycles) / sizeof(sine_wave_four_cycles[0]);
-    for (int i = 0; i < data_len; i++) {
-        xQueueSend(xQueueData, &sine_wave_four_cycles[i], 1000000);
-    }
-
-    while (true) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
 void process_task(void *p) {
+    #define WINDOW_SIZE 5
+    static int window[WINDOW_SIZE] = {0};
+
+    static int idx = 0;
+
+    // Soma corrente de todos os valores no buffer
+    static int sum = 0;
+
+    // Quantas amostras já recebemos (no início, menos de 5)
+    static int samplesCount = 0;
+
     int data = 0;
 
     while (true) {
-        if (xQueueReceive(xQueueData, &data, 100)) {
-            // implementar filtro aqui!
+        if (xQueueReceive(xQueueData, &data, pdMS_TO_TICKS(100))) {
+            
+            sum -= window[idx];       
+            sum += data;             
+            window[idx] = data;     
 
 
+            idx = (idx + 1) % WINDOW_SIZE;
+
+ 
+            if (samplesCount < WINDOW_SIZE) {
+                samplesCount++;
+            }
 
 
-            // deixar esse delay!
+            int filteredValue = sum / samplesCount;
+
+
+            printf("%d \n", filteredValue);
+
             vTaskDelay(pdMS_TO_TICKS(50));
         }
     }
-}
-
-int main() {
-    stdio_init_all();
-
-    xQueueData = xQueueCreate(64, sizeof(int));
-
-    xTaskCreate(data_task, "Data task ", 4096, NULL, 1, NULL);
-    xTaskCreate(process_task, "Process task", 4096, NULL, 1, NULL);
-
-    vTaskStartScheduler();
-
-    while (true)
-        ;
 }
